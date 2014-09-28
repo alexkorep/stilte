e4b952ca20176beed9ee3888c2045b5e8e65cc74
Alexey Korepanov <alexkorep@gmail.com>
2014-09-28 12:42:45 +0200

    more advanced announce test script

--- test_announce.js
+++ test_announce.js
@@ -4,6 +4,18 @@ var infoHash = '0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33';
 var port = 23134;
 
 var dht = new DHT();
-dht.announce(infoHash, port, function(error){
-    console.log('Announced: ' + infoHash + ' on port ' + port + ', error: ' + error);
-});
+var hash = '0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33';
+
+dht.on('ready', function () {
+    var hash = '0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33';
+    dht.lookup(hash, function() {
+        console.log('Lookup finished')
+        dht.announce(hash, port, function(error){
+            console.log('Announced: ' + infoHash + ' on port ' + port + ', error: ' + error);
+        });
+    });
+})
+
+dht.on('peer', function (addr, hash, from) {
+  console.log('found potential peer ' + addr + ' through ' + from + ', hash: ' + hash)
+})
