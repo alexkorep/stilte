fd806ed670d86c68cb84d0520f685d5de832629e
Alexey Korepanov <alexkorep@gmail.com>
2014-10-01 20:37:32 +0200

    extra messages added for announce test script

--- test_announce.js
+++ test_announce.js
@@ -1,17 +1,19 @@
 var DHT = require('bittorrent-dht')
 
-var infoHash = '0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33';
-var port = 23134;
-
 var dht = new DHT();
-var hash = '0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33';
+console.log('Starting...');
 
 dht.on('ready', function () {
+    console.log('Ready, running lookup...');
+
     var hash = '0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33';
+    var port = 23134;
+
     dht.lookup(hash, function() {
-        console.log('Lookup finished')
+
+        console.log('Lookup finished. Announcing peer with hash ' + hash)
         dht.announce(hash, port, function(error){
-            console.log('Announced: ' + infoHash + ' on port ' + port + ', error: ' + error);
+            console.log('Announced: ' + hash + ' on port ' + port + ', error: ' + error);
         });
     });
 })
