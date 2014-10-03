221cee22c3b5719e0984018942ec58877ac813b1
Alexey Korepanov <alexkorep@gmail.com>
2014-10-03 21:22:39 +0200

    listen event fixed

--- test_announce.js
+++ test_announce.js
@@ -3,24 +3,25 @@ var DHT = require('bittorrent-dht')
 var dht = new DHT();
 console.log('Starting...');
 
-dht.listen(20000, function () {
-  console.log('now listening')
-})
+dht.listen(function (port) {
+    console.log('now listening on port ' + port);
 
-dht.on('ready', function () {
-    console.log('Ready, running lookup...');
+    dht.on('ready', function () {
+        console.log('Ready, running lookup...');
 
-    var hash = '0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33';
-    var port = 23134;
+        var hash = '0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33';
+        //var port = 23134;
 
-    dht.lookup(hash, function() {
+        dht.lookup(hash, function() {
 
-        console.log('Lookup finished. Announcing peer with hash ' + hash)
-        dht.announce(hash, port, function(error){
-            console.log('Announced: ' + hash + ' on port ' + port + ', error: ' + error);
+            console.log('Lookup finished. Announcing peer with hash ' + hash)
+            dht.announce(hash, port, function(error){
+                console.log('Announced: ' + hash + ' on port ' + port + ', error: ' + error);
+            });
         });
-    });
+    })
 })
+
 
 dht.on('peer', function (addr, hash, from) {
   console.log('found potential peer ' + addr + ' through ' + from + ', hash: ' + hash)
