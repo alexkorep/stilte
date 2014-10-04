60bc63694108c7a68dbbffb9fef8a9cdfcb03c5d
Alexey Korepanov <alexkorep@gmail.com>
2014-10-04 08:41:44 +0200

    added announce event handling

--- test_announce.js
+++ test_announce.js
@@ -1,6 +1,10 @@
 var DHT = require('bittorrent-dht')
 
-var dht = new DHT();
+var dht = new DHT({
+    // TODO remove it for working with the real life nodes
+    bootstrap: ['127.0.0.1:52163']
+});
+
 console.log('Starting...');
 
 dht.listen(function (port) {
@@ -25,4 +29,10 @@ dht.listen(function (port) {
 
 dht.on('peer', function (addr, hash, from) {
   console.log('found potential peer ' + addr + ' through ' + from + ', hash: ' + hash)
-})
+});
+
+dht.on('announce', function (addr, hash) {
+    // That means that we found a peer which announced the torrent with the
+    // same hash we are looking up
+    console.log('annonce event fired, addr is ' + addr + ', hash is ' + hash);
+});
